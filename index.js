175b0304001d93de91d8160f2f699e3b87ee707f
sparshithNR <nairbali@usc.edu>
2019-11-18 08:59:09 -0800

    Move to use Class
    - update to use class
    - update var with let/const

--- index.js
+++ index.js
@@ -1,46 +1,51 @@
 'use strict';
 
-var Plugin = require('broccoli-plugin');
-var MergeTrees = require('merge-trees');
+const Plugin = require('broccoli-plugin');
+const MergeTrees = require('merge-trees');
 
-module.exports = BroccoliMergeTrees;
-BroccoliMergeTrees.prototype = Object.create(Plugin.prototype);
-BroccoliMergeTrees.prototype.constructor = BroccoliMergeTrees;
-function BroccoliMergeTrees(inputNodes, options) {
-  if (!(this instanceof BroccoliMergeTrees)) return new BroccoliMergeTrees(inputNodes, options);
-  options = options || {};
-  var name = 'broccoli-merge-trees:' + (options.annotation || '');
-  if (!Array.isArray(inputNodes)) {
-    throw new TypeError(name + ': Expected array, got: [' + inputNodes +']');
+class BroccoliMergeTrees extends Plugin{
+  constructor (inputNodes, options = {}) {
+    if (!Array.isArray(inputNodes)) {
+      throw new TypeError(name + ': Expected array, got: [' + inputNodes +']');
+    }
+    super(inputNodes, {
+      persistentOutput: true,
+      needsCache: false,
+      annotation: options.annotation
+    });
+
+    let name = 'broccoli-merge-trees:' + (options.annotation || '');
+
+    this.inputNodes = inputNodes;
+    this.options = options;
   }
-  Plugin.call(this, inputNodes, {
-    persistentOutput: true,
-    needsCache: false,
-    annotation: options.annotation
-  });
-  this.inputNodes = inputNodes;
-  this.options = options;
+
+  build() {
+    if (this.mergeTrees == null) {
+      // Defer instantiation until the first build because we only
+      // have this.inputPaths and this.outputPath once we build.
+      this.mergeTrees = new MergeTrees(this.inputPaths, this.outputPath, {
+        overwrite: this.options.overwrite,
+        annotation: this.options.annotation
+      });
+    }
+
+    try {
+      this.mergeTrees.merge();
+    } catch(err) {
+      if (err !== null && typeof err === 'object') {
+        let nodesList = this.inputNodes.map((node, i) => `  ${i+1}.  ${node.toString()}`).join('\n');
+        let message = `${this.toString()} error while merging the following:\n${nodesList}`;
+
+        err.message = `${message}\n${err.message}`;
+      }
+      throw err;
+    }
+  }
 }
 
-BroccoliMergeTrees.prototype.build = function() {
-  if (this.mergeTrees == null) {
-    // Defer instantiation until the first build because we only
-    // have this.inputPaths and this.outputPath once we build.
-    this.mergeTrees = new MergeTrees(this.inputPaths, this.outputPath, {
-      overwrite: this.options.overwrite,
-      annotation: this.options.annotation
-    });
-  }
-
-  try {
-    this.mergeTrees.merge();
-  } catch(err) {
-    if (err !== null && typeof err === 'object') {
-      let nodesList = this.inputNodes.map((node, i) => `  ${i+1}.  ${node.toString()}`).join('\n');
-      let message = `${this.toString()} error while merging the following:\n${nodesList}`;
-
-      err.message = `${message}\n${err.message}`;
-    }
-    throw err;
-  }
+module.exports = function mergeTrees(...params) {
+  return new BroccoliMergeTrees(...params);
 };
+
+module.exports.BroccoliMergeTrees = BroccoliMergeTrees;
